e55ea54296786631603aafa7a46b3333b19a11ed
Morgan Jones <me@morganuk.ga>
2022-08-28 03:51:21 +0100

    Basic game functionality

--- src/games/tic-tac-toe.mjs
+++ src/games/tic-tac-toe.mjs
@@ -1,37 +1,92 @@
 import readline from "readline"
+import chalk from "chalk"
 
 // Initiate readline
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 
 export async function game() {
-  
   const board = new Board();
-  board.printBoard()
+  await board.startGame();
 }
 
 class Board {
-  s = {
-    1: "1",
-    2: "2",
-    3: "3",
-    4: "4",
-    5: "5",
-    6: "6",
-    7: "7",
-    8: "8",
-    9: "9",
+  currentPlayer = [1, "x"]
+  s = {}
+
+  constructor() {
+    for (let i = 1; i <= 9; i++) {
+      this.s[i] = i.toString()
+    }
   }
 
-  constructor() {}
+  async startGame() {
+    this.printBoard()
+    await this.nextMove();
+  }
 
   printBoard() {
+    const boardEntry = (number) => {
+      const value = this.s[number];
+      if (/[0-9]/g.test(value)) return chalk.bold.green(`${this.s[`${number}`]}`)
+      else if (value === "x") return chalk.bold.yellow(`x`)
+      else if (value === "o") return chalk.bold.yellow(`o`)
+    }
+
     console.log(`-------------`)
-    console.log(`| ${this.s["1"]} | ${this.s["2"]} | ${this.s["3"]} |`)
+    console.log(`| ${boardEntry(1)} | ${boardEntry(2)} | ${boardEntry(3)} |`)
     console.log(`+---+---+---+`)
-    console.log(`| ${this.s["4"]} | ${this.s["5"]} | ${this.s["6"]} |`)
+    console.log(`| ${boardEntry(4)} | ${boardEntry(5)} | ${boardEntry(6)} |`)
     console.log(`+---+---+---+`)
-    console.log(`| ${this.s["7"]} | ${this.s["8"]} | ${this.s["9"]} |`)
+    console.log(`| ${boardEntry(7)} | ${boardEntry(8)} | ${boardEntry(9)} |`)
     console.log(`-------------`)
+  }
+
+  validateEntries() {
+    // Check if any player has won the round, if there's a tie or the game should continue
+
+    /*
+    if (square 1 = square 2 = square 3, etc, etc) {
+      return "win"
+    }
+    if (all squares are either `x` or `o` (tie)) {
+      return "tie"
+    }
+    else {
+      return "continue"
+    }
+    */
+  }
+
+  markSquare(square, player) {
+    this.s[square] = player
+  }
+  
+  checkIfSquareFree(square) {
+    if (this.s[square] === square) {
+      return true
+    }
+    return false
+  }
+
+  async nextMove() {
+    const squareToMark = await getUserInput(`Player ${this.currentPlayer[0]}, which square do you want to mark?`);
+
+    if (!(squareToMark >= 1) || !(squareToMark <= 9) || !/[0-9]/g.test(squareToMark)) {
+      console.log("Please enter a valid number!");
+      await this.getNextInput();
+    }
+
+    const isFree = this.checkIfSquareFree(squareToMark);
+
+    if (!isFree) {
+      console.log("That square is not free...");
+      await this.getNextInput()
+      return;
+    }
+
+    this.markSquare(squareToMark, this.currentPlayer[1])
+
+    this.currentPlayer = [2, "o"]
   }
 }
 
